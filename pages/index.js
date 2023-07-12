da87762c5d875d82b456861e515a698b38895ca1
Thau Nguyen <nghoangthau@gmail.com>
2023-07-12 20:51:10 +0700

    add mui

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
-import Link from "next/link";
-import { client } from "../libs/client";
+import Link from 'next/link';
+import { client } from '../libs/client';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import Badge from '@mui/material/Badge';
+import MailIcon from '@mui/icons-material/Mail';
 
 export default function Home({ blog }) {
   return (
     <div>
+      <AppBar />
+      <Button>MUI Button</Button>
+      <Badge badgeContent={4} color="primary">
+        <MailIcon color="action" />
+      </Badge>
+      <p>Page Router</p>
       <ul>
         {blog.map((blog) => (
           <li key={blog.id}>
@@ -11,6 +21,7 @@ export default function Home({ blog }) {
           </li>
         ))}
       </ul>
+      <p>App Router</p>
       <ul>
         {blog.map((blog) => (
           <li key={blog.id}>
@@ -23,7 +34,7 @@ export default function Home({ blog }) {
 }
 
 export const getStaticProps = async () => {
-  const data = await client.get({ endpoint: "blog" });
+  const data = await client.get({ endpoint: 'blog' });
 
   return {
     props: {
